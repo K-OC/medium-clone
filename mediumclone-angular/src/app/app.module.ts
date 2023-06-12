711ed658477370c5235f703be918b3e776cf4bae
Kieran O'Connell <kieranpoconnell@gmail.com>
2023-06-12 20:37:06 -0230

    completed registration component

--- mediumclone-angular/src/app/app.module.ts
+++ mediumclone-angular/src/app/app.module.ts
@@ -1,13 +1,29 @@
 import {NgModule} from '@angular/core'
 import {BrowserModule} from '@angular/platform-browser'
+import {StoreModule} from '@ngrx/store'
+import {StoreDevtoolsModule} from '@ngrx/store-devtools'
+import {environment} from 'src/environments/environment'
 
 import {AppRoutingModule} from './app-routing.module'
 import {AppComponent} from './app.component'
 import {AuthModule} from 'src/app/auth/auth.module'
+import {HttpClientModule} from '@angular/common/http'
+import {EffectsModule} from '@ngrx/effects'
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, AuthModule],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule,
+    AuthModule,
+    StoreModule.forRoot({}),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25,
+      logOnly: environment.production,
+    }),
+    EffectsModule.forRoot([]),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- mediumclone-angular/src/app/auth/auth.module.ts
+++ mediumclone-angular/src/app/auth/auth.module.ts
@@ -1,12 +1,29 @@
 import {CommonModule} from '@angular/common'
 import {NgModule} from '@angular/core'
 import {RouterModule} from '@angular/router'
+import {RegisterEffect} from 'src/app/auth/store/effects/register.effect'
+import {ReactiveFormsModule} from '@angular/forms'
+import {StoreModule} from '@ngrx/store'
+
+import {reducers} from 'src/app/auth/store/reducers'
 import {RegisterComponent} from 'src/app/auth/components/register/register.component'
+import {AuthService} from 'src/app/auth/services/auth.service'
+import {EffectsModule} from '@ngrx/effects'
+import {BackendErrorMessagesModule} from '../shared/modules/backendErrorMessages/backendErrorMessages.module'
+import {PersistanceService} from '../shared/services/persistance.service'
 
 const routes = [{path: 'register', component: RegisterComponent}]
 
 @NgModule({
-  imports: [CommonModule, RouterModule.forChild(routes)],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes),
+    ReactiveFormsModule,
+    StoreModule.forFeature('auth', reducers),
+    EffectsModule.forFeature([RegisterEffect]),
+    BackendErrorMessagesModule,
+  ],
   declarations: [RegisterComponent],
+  providers: [AuthService, PersistanceService],
 })
 export class AuthModule {}
